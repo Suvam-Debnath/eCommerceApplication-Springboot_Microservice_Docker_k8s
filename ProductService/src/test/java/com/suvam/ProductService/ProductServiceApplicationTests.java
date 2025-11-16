b3c7bf342dff45a8141bed6076f22f4896a63f28
Suvam Debnath <debnathsuvam322@gmail.com>
2025-11-16 09:32:29 +0530

    product service initial commit

---
+++ ProductService/src/test/java/com/suvam/ProductService/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suvam.ProductService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
